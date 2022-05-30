12ff86c68a0e1fc5906657b40b55f2d8c2d78d28
YR-Johnson <eljonybarra@gmail.com>
2022-05-30 17:48:15 -0300

    modifique autos.js

--- autos.js
+++ autos.js
@@ -1,25 +1,36 @@
 const autos = [
     {
-        marca = "Ford";
-        modelo = "Fiesta";
-        precio = 150000;
-        km = 200;
-        color = "Azul";
-        coutas = 12;
-        año = 2019;
-        patente = "APL123";
-        vendido = false;
-    }
+        marca : "Ford",
+        modelo : "Fiesta",
+        precio : 150000,
+        km : 200,
+        color : "Azul",
+        coutas : 12,
+        año : 2019,
+        patente : "APL123",
+        vendido : false,
+    },
     {
-        marca = "Toyota";
-        modelo = "Corolla";
-        precio = 850000;
-        km = 0;
-        color = "Blanco";
-        coutas = 14;
-        año = 2019;
-        patente = "JLK116";
-        vendido = false;
+        marca : "Toyota",
+        modelo : "Corolla",
+        precio : 850000,
+        km : 0,
+        color : "Blanco",
+        coutas : 14,
+        año : 2019,
+        patente : "JLK116",
+        vendido : false,
+    },
+    {
+        marca : "Ford",
+        modelo : "Ranger",
+        precio : 2550000,
+        km : 200000,
+        color : "Blanco",
+        coutas : 14,
+        año : 2008,
+        patente : "FJV308",
+        vendido : false,
     }
 ]
 module.exports = autos;
